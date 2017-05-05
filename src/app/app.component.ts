c90295a4c010786af16434579dfd6e03f6dc79f4
Tim <Tim.mouse@gmail.com>
2017-05-06 07:33:57 +0800

    info page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { TagService } from './tag.service';
+import { Tag } from './tag';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ TagService ],
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  errorMessage: string;
+  heroes: Tag[];
+  mode = 'Observable';
+  constructor (private heroService: TagService) {}
+  ngOnInit() { this.getHeroes(); }
+  getHeroes() {
+    this.heroService.getHeroes()
+                     .subscribe(
+                       heroes => this.heroes = heroes,
+                       error =>  this.errorMessage = <any>error);
+  }
+  addHero(uid: number) {
+    if (!uid) { return; }
+    this.heroService.create(uid)
+                     .subscribe(
+                       hero  => this.heroes.push(hero),
+                       error =>  this.errorMessage = <any>error);
+  }
 }
